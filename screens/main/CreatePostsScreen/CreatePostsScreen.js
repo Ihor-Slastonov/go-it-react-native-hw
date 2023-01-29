cda553b957afa4c81c2eb7e7aa199553f8577da1
Ihor-Slastonov <jammik000@gmail.com>
2023-01-29 12:55:20 +0200

    Update CreatePostsScreen.js

--- screens/main/CreatePostsScreen/CreatePostsScreen.js
+++ screens/main/CreatePostsScreen/CreatePostsScreen.js
@@ -3,7 +3,9 @@ import { View, Text, StyleSheet } from 'react-native';
 export const CreatePostScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>CreatePostScreen</Text>
+      <View style={styles.imageContainer}>
+
+      </View>
     </View>
   );
 };
@@ -11,8 +13,12 @@ export const CreatePostScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#fff',
+    paddingHorizontal: 16,
   },
+  imageContainer: {
+    width: '100%',
+    height: 240,
+    backgroundColor: '#F6F6F6',
+  }
 });
