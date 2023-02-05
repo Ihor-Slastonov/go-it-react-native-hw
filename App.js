33e930bda93b236daaec78dae5cc6c25f8764dcd
Ihor-Slastonov <jammik000@gmail.com>
2023-02-05 11:32:45 +0200

    update

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { store } from './redux/store';
-import { ToastProvider } from 'react-native-toast-notifications';
 import * as Font from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
@@ -36,26 +35,8 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <ToastProvider
-        placement="top"
-        duration={5000}
-        animationType='slide-in'
-        animationDuration={500}
-        successColor="green"
-        dangerColor="red"
-        warningColor="orange"
-        // icon={<Icon />}
-        // successIcon={<SuccessIcon />}
-        // dangerIcon={<DangerIcon />}
-        // warningIcon={<WarningIcon />}
-        textStyle={{ fontSize: 20 }}
-        offsetTop={120}
-        offsetBottom={40}
-        swipeEnabled={true}
-      >
         <Home />
         <StatusBar style="auto" />
-      </ToastProvider>
     </Provider>
   );
 }
--- screens/main/Home/Home.js
+++ screens/main/Home/Home.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { ToastProvider } from 'react-native-toast-notifications';
 
-import { useEffect} from 'react';
+import { useEffect } from 'react';
 import { useRoute } from '../../../router';
 import { useSelector, useDispatch } from 'react-redux';
 import { authStateChanged } from '../../../redux/auth/operations';
@@ -11,9 +12,28 @@ export const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(authStateChanged());
-    
   }, []);
   const routing = useRoute(stateChange);
 
-  return <NavigationContainer>{routing}</NavigationContainer>;
+  return (
+    <ToastProvider
+      placement="top"
+      duration={5000}
+      animationType="slide-in"
+      animationDuration={500}
+      successColor="green"
+      dangerColor="red"
+      warningColor="orange"
+      // icon={<Icon />}
+      // successIcon={<SuccessIcon />}
+      // dangerIcon={<DangerIcon />}
+      // warningIcon={<WarningIcon />}
+      textStyle={{ fontSize: 20 }}
+      offsetTop={120}
+      offsetBottom={40}
+      swipeEnabled={true}
+    >
+      <NavigationContainer>{routing}</NavigationContainer>
+    </ToastProvider>
+  );
 };
