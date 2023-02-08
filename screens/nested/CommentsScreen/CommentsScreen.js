f12365c04128e7e2effbc5c07bb8034d448263a4
Ihor-Slastonov <jammik000@gmail.com>
2023-02-08 19:24:36 +0200

    Update CommentsScreen.js

--- screens/nested/CommentsScreen/CommentsScreen.js
+++ screens/nested/CommentsScreen/CommentsScreen.js
@@ -1,19 +1,59 @@
-import { View, Text, StyleSheet, Image, TextInput } from 'react-native';
+import { useState, useEffect } from 'react';
+
+import {
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+import { AntDesign } from '@expo/vector-icons';
 
 export const CommentsScreen = ({ route }) => {
+  const [isKeyboardShown, setIsKeyboardShown] = useState(false);
+  const [comment, setComment] = useState('');
   const { photo } = route.params;
+
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener('keyboardDidShow', () => {
+      setIsKeyboardShown(true);
+    });
+    const hideSubscription = Keyboard.addListener('keyboardDidHide', () => {
+      setIsKeyboardShown(false);
+    });
+
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
+
+  const handleSetComment = text => setComment(text);
+
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: photo }} style={styles.photo} />
-      <View style={{flex: 1}}></View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.inputComment}
-          placeholder="Write comment"
-          placeholderTextColor={'#BDBDBD'}
-        />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Image source={{ uri: photo }} style={styles.photo} />
+        <View style={{ flex: 1 }}></View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={comment}
+            onChangeText={handleSetComment}
+            style={styles.inputComment}
+            placeholder="Write comment"
+            placeholderTextColor={'#BDBDBD'}
+          />
+          <TouchableOpacity style={styles.submit}>
+            <AntDesign name="arrowup" size={24} color="#ffffff" />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -22,6 +62,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 16,
     backgroundColor: '#fff',
+    paddingBottom: 16,
   },
   photo: {
     marginTop: 32,
@@ -30,6 +71,7 @@ const styles = StyleSheet.create({
     height: 240,
     borderRadius: 8,
   },
+  inputContainer: {},
   inputComment: {
     padding: 16,
     width: '100%',
@@ -41,5 +83,16 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto-Regular',
     fontSize: 16,
     color: '#212121',
+  },
+  submit: {
+    position: 'absolute',
+    top: 8,
+    right: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 34,
+    height: 34,
+    backgroundColor: '#FF6C00',
+    borderRadius: 50,
   },
 });
