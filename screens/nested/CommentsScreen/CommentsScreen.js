5228f0080f4101a77ede9c19b0ef8779ffe0590c
Ihor-Slastonov <jammik000@gmail.com>
2023-02-08 19:13:53 +0200

    Update CommentsScreen.js

--- screens/nested/CommentsScreen/CommentsScreen.js
+++ screens/nested/CommentsScreen/CommentsScreen.js
@@ -1,10 +1,18 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput } from 'react-native';
 
 export const CommentsScreen = ({ route }) => {
   const { photo } = route.params;
   return (
     <View style={styles.container}>
       <Image source={{ uri: photo }} style={styles.photo} />
+      <View style={{flex: 1}}></View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputComment}
+          placeholder="Write comment"
+          placeholderTextColor={'#BDBDBD'}
+        />
+      </View>
     </View>
   );
 };
@@ -13,9 +21,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 16,
+    backgroundColor: '#fff',
   },
   photo: {
+    marginTop: 32,
+    marginBottom: 32,
     width: '100%',
     height: 240,
+    borderRadius: 8,
+  },
+  inputComment: {
+    padding: 16,
+    width: '100%',
+    height: 50,
+    borderColor: '#E8E8E8',
+    borderWidth: 1,
+    borderRadius: 100,
+    backgroundColor: '#F6F6F6',
+    fontFamily: 'Roboto-Regular',
+    fontSize: 16,
+    color: '#212121',
   },
 });
